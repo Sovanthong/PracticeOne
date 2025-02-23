3b6c67b4700278a59c9cc0c11fff5b746d5e3a04
Sovanthong <sovanthongsoung@gmail.com>
2025-02-23 11:24:54 +0700

    Update duoloop.cpp

--- duoloop.cpp
+++ duoloop.cpp
@@ -1,11 +1,1 @@
-#include<iostream>
-using namespace std;
-int main (){
-    system("cls")
-int i = 1;
-do{
-    cout<<i<<".this is inside duo while loop"<<endl;
-    i++;
-}while (i>10);
-    return 0;
-}
+
