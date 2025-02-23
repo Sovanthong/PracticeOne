be942af2fde364af24db5ade3d86ecd3cb8bdfc9
Sovanthong <sovanthongsoung@gmail.com>
2025-02-23 11:24:37 +0700

    Update finalloop.cpp

--- finalloop.cpp
+++ finalloop.cpp
@@ -1,15 +1,1 @@
-#include<iostream>
-using namespace std;
-int main(){
-    system("cls");
-for (int i = 1 ; i<=10 , i++ ;){
-cout<<i<<".inside for loop."<<endl;
-if (i==5)break;
-}
-for (int k = 1 ; k<=10; k++){
-    if (k==5|| k==8 )continue;
-    cout<<k<<".inside while loop."<<endl;
-}
-cout<<".THIS IS THE LAST LINE."<<endl;
-    return 0;
-}
+
